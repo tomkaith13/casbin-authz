212206ea0238f2102d1e4ac4584712bcf5b453ad
Bibin Thomas <bthomas@league.com>
2024-06-25 18:29:36 -0400

    Added a basic policy enforcer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/casbin/casbin"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -16,7 +17,26 @@ func main() {
 
 	// Dummy Endpoint
 	r.Get("/dummy", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("This is a dummy endpoint!!!!"))
+
+		e := casbin.NewEnforcer("./abac.conf", "./policy.csv")
+		if e == nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		sub := "alice" // the user that wants to access a resource.
+		obj := "claim" // the resource that is going to be accessed.
+		act := "write" // the operation that the user performs on the resource.
+
+		if res := e.Enforce(sub, obj, act); res {
+			// permit alice to read data1
+			w.Write([]byte("This is a dummy allowed endpoint!!!!"))
+			w.WriteHeader(http.StatusOK)
+
+		} else {
+			// deny the request, show an error
+			http.Error(w, "This is a dummy restricted endpoint!!!!", http.StatusForbidden)
+			return
+		}
 	})
 
 	// Start Server
