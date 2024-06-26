b9f9a69463f05abc50336b2b968e4aad3573fab2
Bibin Thomas <bthomas@league.com>
2024-06-26 12:27:18 -0400

    added abac rule and support

--- go.mod
+++ go.mod
@@ -2,12 +2,13 @@ module github.com/tomkaith13/casbin-authz
 
 go 1.22.2
 
-require github.com/go-chi/chi/v5 v5.0.14
-
-require github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
-
 require (
 	github.com/casbin/casbin v1.9.1
+	github.com/go-chi/chi/v5 v5.0.14
+)
+
+require (
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
 	github.com/casbin/casbin/v2 v2.97.0 // indirect
 	github.com/casbin/govaluate v1.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/casbin/casbin"
+	"github.com/casbin/casbin/v2"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -18,8 +18,8 @@ func main() {
 	// Dummy Endpoint
 	r.Get("/dummy", func(w http.ResponseWriter, r *http.Request) {
 
-		e := casbin.NewEnforcer("./abac.conf", "./policy.csv")
-		if e == nil {
+		e, err := casbin.NewEnforcer("./model.conf", "./policy.csv")
+		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
@@ -28,7 +28,7 @@ func main() {
 		obj := "claim" // the resource that is going to be accessed.
 		act := "read"  // the operation that the user performs on the resource.
 
-		if res := e.Enforce(sub, obj, act); res {
+		if res, _ := e.Enforce(sub, obj, act); res {
 			// permit alice to read claim
 			w.Write([]byte("This is a dummy allowed endpoint!!!!"))
 			w.WriteHeader(http.StatusOK)
@@ -36,6 +36,36 @@ func main() {
 		} else {
 			// deny the request, show an error
 			http.Error(w, "This is a dummy restricted endpoint!!!!", http.StatusForbidden)
+			return
+		}
+	})
+
+	// Dummy Endpoint
+	r.Get("/abac", func(w http.ResponseWriter, r *http.Request) {
+		type Subject struct {
+			Name  string
+			Group string
+		}
+
+		e, err := casbin.NewEnforcer("./abac.conf", "./abac_policy.csv")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		subj := Subject{
+			Name:  "alice",
+			Group: "caregiver",
+		}
+
+		if res, _ := e.Enforce(subj, "claim", "read"); res {
+			// permit alice to read claim
+			w.Write([]byte("This is a abac allowed endpoint!!!!"))
+			w.WriteHeader(http.StatusOK)
+
+		} else {
+			// deny the request, show an error
+			http.Error(w, "This is a abac restricted endpoint!!!!", http.StatusForbidden)
 			return
 		}
 	})
